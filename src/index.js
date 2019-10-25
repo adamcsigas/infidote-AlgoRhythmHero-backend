b0be678f5c4f3477301f3cde263821a74074ce6c
adamcsigas <csigasadam@gmail.com>
2019-10-25 13:57:55 +0200

    feat(server) server is up and running

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const PORT = 3000;
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const router = require('./routes/index');
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Port is listening on ${PORT}`); // eslint-disable-line
+});
+
+module.exports = app;
